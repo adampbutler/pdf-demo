ba40f93aa1de1652ec7e05627d8314b81c579fed
Adam Butler <adam.butler@compono.com>
2023-08-24 15:08:57 +1000

    Remove junk

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { createRef } from 'react';
+import { jsPDF } from "jspdf";
+
+function generate(ref) {
+  const doc = new jsPDF();
+
+  doc.html(ref, {
+   callback: function (doc) {
+     doc.save();
+   },
+});
+}
 
 function App() {
+  const ref = createRef();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => generate(ref?.current)}>Generate</button>
       </header>
+      <div style={{ display: 'none' }}>
+        <div ref={ref}>
+          Hello world1
+        </div>
+      </div>
     </div>
   );
 }
